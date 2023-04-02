58553b9421113249920e359561bdca0003c61fe6
ruudyaa <martarudnytska26@gmail.com>
2023-04-02 19:20:58 +0300

    Adds second task.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,38 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+player
+  .setColor('#fff0f5')
+  .then(function (color) {
+    // the color that was set
+  })
+  .catch(function (error) {
+    // an error occurred setting the color
+  });
