0988ba55fd0c6bc60f38146fd63e26aded33c71e
ruudyaa <martarudnytska26@gmail.com>
2023-04-04 00:02:27 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,7 @@ import throttle from 'lodash.throttle';
 const LOCAL_KEY = 'feedback-form-state';
 let formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
 
-form = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(storageFormData, 500));
 form.addEventListener('submit', onFormSubmit);
